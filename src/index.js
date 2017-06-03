feb3fca4be953553985b366873a6cd0f7984cc45
Kung Wang <kung.wang@totvs.com>
2017-06-03 08:09:27 -0700

    move inline styles into stylesheet

--- src/index.js
+++ src/index.js
@@ -26,23 +26,23 @@ export default class App extends Component {
         </TouchableOpacity>
         <Modal isVisible={this.state.isModalVisible}>
           <View style={styles.modalContent}>
-            <View style={{justifyContent: 'center', alignItems: 'center', marginBottom: 8}}>
+            <View style={styles.modalImage}>
               <Image style={{width: this.state.width, height: this.state.height}} resizeMode={Image.resizeMode.cover} source={{uri: this.state.imgsrc}} />
             </View>
             <Text style={{marginBottom: 16}}>{this.state.title}</Text>
 
-            <View style={styles.actionRow}>
+            <View style={styles.modalRow}>
               <Text style={{marginRight: 8}}>Price</Text>
               <Text style={{fontWeight: 'bold'}}>{this.state.price}</Text>
             </View>
-            <View style={styles.actionRow}>
+            <View style={styles.modalRow}>
               <Text>Alert me when price is lower than</Text>
               <TextInput
-                style={{marginLeft: 8, height: 40, flex: 1, borderColor: 'transparent', backgroundColor: '#f3f3f3', borderWidth: 1, borderRadius: 2}}
+                style={styles.modalInput}
                 keyboardType={'numeric'}
               />
             </View>
-            <View style={styles.actionRow}>
+            <View style={styles.modalRow}>
 
               <TouchableOpacity onPress={this._hideModal}>
                 <View style={[styles.modalButton, {marginRight: 8}]}>
@@ -73,7 +73,12 @@ var styles = StyleSheet.create({
     marginTop: 20,
 
   },
-  actionRow: {
+  modalImage: {
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    marginBottom: 8
+  },
+  modalRow: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingTop: 4,
@@ -95,7 +100,16 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 4,
     borderColor: 'rgba(0, 0, 0, 0.1)',
-  }
+  },
+  modalInput: {
+    flex: 1, 
+    marginLeft: 8, 
+    height: 40, 
+    borderColor: 'transparent', 
+    backgroundColor: '#f3f3f3', 
+    borderWidth: 1, 
+    borderRadius: 2
+  },
 });
 
 
